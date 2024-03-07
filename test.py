f804fb6186ec371b293463920a1e93b16d369343
TheCreatorrr333 <thecreatorrr333@gmail.com>
2024-03-07 20:47:58 +0100

    Initial Commit 2

---
+++ test.py
@@ -0,0 +1,22 @@
+import sys
+
+import tcrutils as tcr
+from tcrutils import console
+
+import nekoweb as nw
+
+console.debug(sorted(a := [x for x in globals() if not x.startswith('_')]), len(a)); del a
+console.log(f"{tcr.c}Running on Python %s.%s" % sys.version_info[:2])
+
+
+def test_test():
+  console(nw)
+
+
+if True:  # Test setup
+  for k, v in globals().copy().items():  # Decorate each test_... function with the @tcr.test decorator
+    if k.startswith('test_'):
+      globals()[k] = tcr.test(v)
+
+if True:
+  test_test()
