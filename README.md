7336aa39dbeb07df4767510adcf74e7589b00094
TheCreatorrr333 <thecreatorrr333@gmail.com>
2024-03-19 18:51:08 +0100

    rather initial version, site info rather working

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Nekoweb API wrapper
+
+- [ ] XSync
+- [x] Httpx
+- [ ] ask chat gpt for forwarding the self parameter to a dot-accessed thing
--- test.py
+++ test.py
@@ -1,17 +1,23 @@
+import asyncio
 import sys
 
 import tcrutils as tcr
-from tcrutils import console
+from colored import Back, Fore, Style
+from tcrutils import asshole, console, rashole
 
-import nekoweb as nw
+import nekoweb as nkw
 
 console.debug(sorted(a := [x for x in globals() if not x.startswith('_')]), len(a))
-del a
-console.log(f'{tcr.c}Running on Python %s.%s' % sys.version_info[:2])
+console.log(f'{Style.reset}Running on Python %s.%s' % sys.version_info[:2])
+tcr.WarningCatcher()
+
+NCL = nkw.Client(api_key=tcr.get_token('API_KEY.txt'))
 
 
-def test_test():
-  console(nw)
+async def test_client():
+  console(NCL)
+
+  console(await NCL.site_info('asdfsdgsdfirc'))
 
 
 if True:  # Test setup
@@ -19,5 +25,11 @@ if True:  # Test setup
     if k.startswith('test_'):
       globals()[k] = tcr.test(v)
 
-if True:
-  test_test()
+
+async def main():  # Tests
+  await test_client()
+  pass
+
+
+if __name__ == '__main__':
+  asyncio.run(main())
